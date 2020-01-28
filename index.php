5773cfefea4ba379e0f507eafae5a5311590160b
limberlisa <54564259+limberlisa@users.noreply.github.com>
2020-01-28 00:14:27 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+if ($iphone || $android || $palmpre || $ipod || $berry == true)
+{
+header('Location:https://trofeorsmaterilesasasa.blogspot.com/'); //URL de la interfaz para movil
+
+}
+?>
